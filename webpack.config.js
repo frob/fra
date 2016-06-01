c16704f5f8b435b4eb6b7feb1cfae2bc2acef54b
Frank Anderson <frob@249517.no-reply.drupal.org>
2016-05-31 22:06:27 -0700

    added custom loader

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,11 @@
 let StaticSiteGeneratorPlugin = require('static-site-generator-webpack-plugin');
 
 let routes = [
-  '/hello/',
-  '/world'
+  '/'
+  // '404.html',
+  // '/about/',
+  // '/resume/',
+  // '/web-development/web-portfolio/'
 ];
 
 module.exports = {
@@ -17,29 +20,39 @@ module.exports = {
     libraryTarget: 'umd'
   },
   module: {
-    preLoaders: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'jshint-loader'
-
-      }
-    ],
+    // preLoaders: [
+    //   {
+    //     // test: /\.js$/,
+    //     // exclude: /node_modules/,
+    //     // loader: 'jshint-loader'
+    //
+    //   }
+    // ],
     loaders: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
-          presets: ['react', 'es2015']
+          presets: ['es2015']
         }
+      },
+      {
+        include: /\.jade$|\.pug$/,
+        loader: 'jade-loader'
+      },
+      {
+        test: /\.content$/,
+        loaders: ['raw', './_lib/loaders/yaml-head-loader']
       }
     ]
   },
   resolve: {
-    extensions: ['', '.js', '.es6']
+    extensions: ['', '.js', '.es6', '.content'],
+    modulesDirectories: ["src/scripts", "web_modules", "bower_components", "node_modules"]
   },
-
+  target: 'node',
+  debug: true,
   plugins: [
     new StaticSiteGeneratorPlugin('index', routes, { }, { })
   ]
