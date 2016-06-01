77848029c05bb18217c51b0b665a9b9fe5d34644
Frank Anderson <frob@249517.no-reply.drupal.org>
2016-05-31 22:08:52 -0700

    only keeping logger arround so I don't have to look up the api later

--- logger.js
+++ logger.js
@@ -1,8 +1,1 @@
-let checkName= (firstName, lastName) => {
- if(firstName !== 'nader' || lastName !== 'dabit') {
-   console.log('You are not Nader Dabit');
- } else {
-    console.log('You are Nader Dabit');
-  }
-}
-checkName('nader', 'jackson');
+console.log("loaded");
