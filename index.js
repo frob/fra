c284fd16f6cbf45519f8afe933410e989eded3e3
Frank Anderson <frob@249517.no-reply.drupal.org>
2016-05-31 22:08:15 -0700

    added test stuff to index

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 require('./logger');
-
-// Client render (optional):
-if (typeof document !== 'undefined') {
-  // Client render code goes here...
-}
-
+// let yamlParse = require('./_lib/loaders/yaml-head-loader.js');
+let content = require('./content/index.content');
+console.log(content);
+// console.log(yamlParse(stuff));
 // Exported static site renderer:
 module.exports = function render(locals, callback) {
-  callback(null, '<!doctype html><html><body>Hello');
+  let data = {
+    localJS: 'index.js',
+    body: content.tail,
+    vars: 'stuff',
+    js: "stuff"
+  };
+  var template = require('./theme/templates/html.pug');
+  callback(null, template(data));
 };
