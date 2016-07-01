9fac7e2dcbab98e4f7fef61a03c5055d603dbd5a
Frank Anderson <frob@249517.no-reply.drupal.org>
2016-07-01 00:16:31 -0700

    guess pug head loader wasn't work as intended. I had to fix the content parsing

--- src/loaders/pug-head-loader.js
+++ src/loaders/pug-head-loader.js
@@ -6,7 +6,6 @@ var pug = require('pug');
 module.exports = function(content) {
   //@TODO Add template checking here. Allow meta to specify a wrapping template.
   const processor = pug.compile(content.tail);
-  content = processor(content.meta);
-
+  content.content = processor(content.meta);
   return content;
 };
