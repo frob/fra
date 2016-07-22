113e14fa253f136a86935041d574815f85f9e310
Frank Anderson <frob@249517.no-reply.drupal.org>
2016-07-22 14:42:24 -0700

    removed comment todo

--- src/loaders/pug-head-loader.js
+++ src/loaders/pug-head-loader.js
@@ -4,7 +4,6 @@ var yaml = require('js-yaml');
 var pug = require('pug');
 
 module.exports = function(content) {
-  //@TODO Add template checking here. Allow meta to specify a wrapping template.
   const processor = pug.compile(content.tail);
   content.content = processor(content.meta);
   return content;
