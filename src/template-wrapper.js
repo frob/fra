be7a67d65a7871ddb262a28bc391aae71a49b060
Frank Anderson <frob@249517.no-reply.drupal.org>
2016-07-22 14:31:18 -0700

    added a new line to the text that is inserted into the template

--- src/template-wrapper.js
+++ src/template-wrapper.js
@@ -12,7 +12,10 @@ module.exports = function(contentObject) {
   if (templateName !== null) {
     var template = fs.readFileSync(templateName, 'utf-8');
     const processor = pug.compile(template);
-    contentObject.tail = processor(Object.assign(contentObject.meta, {tail: contentObject.tail}));
+    // Here we need to add a new line to the tail member to ensure that any new
+    // pug or markdown is on a new line and not continuing off of the old line.
+    // If this isn't done then the first line will not be parsed.
+    contentObject.tail = processor(Object.assign(contentObject.meta, {tail: "\n" + contentObject.tail}));
   }
 
   return contentObject;
