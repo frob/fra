bd3618bd8772d442b6ab25ce1a0dbc47a391bea4
Frank Anderson <frob@249517.no-reply.drupal.org>
2016-07-22 15:22:40 -0700

    updated the template wrapper with some better documentation

--- src/loaders/template-wrapper-loader.js
+++ src/loaders/template-wrapper-loader.js
@@ -1,16 +1,41 @@
+/**
+ * @file Checks the content Object for a wrapping template file name and
+ *   attempts to wrap the content tail in that pug template. This is intended to
+ *   be a loader for webpack.
+ * @author Frank Robert Anderson
+ */
+
 "use strict";
 
-var pug = require('pug');
-var fs = require('fs');
+const pug = require('pug');
+const fs = require('fs');
 
+/**
+ * Returns content wrapped in template (if) specified in meta.template.
+ *
+ * @param {Object} contentObject
+ *  Content Object, thing getting processed by loader.
+ *
+ * @param {Object} [contentObject.meta]
+ *  Object that contains meta information and variable for use in the template.
+ *
+ * @param {String} [contentObject.meta.template=undefined]
+ *  Optional name of the template that the content will be wrapped.
+ *
+ * @param {String} [contentObject.tail]
+ *  Tail content that will be processed in the next pass, could be pug/markdown.
+ *
+ * @return {String}
+ */
 module.exports = function(contentObject) {
-  let templateName =
+  // Get the name of the template from the meta data.
+  const templateName =
     (typeof contentObject.meta.template === 'undefined')
     ? null
     : `./theme/templates/${contentObject.meta.template}.pug`;
 
   if (templateName !== null) {
-    var template = fs.readFileSync(templateName, 'utf-8');
+    const template = fs.readFileSync(templateName, 'utf-8');
     const processor = pug.compile(template);
     // Here we need to add a new line to the tail member to ensure that any new
     // pug or markdown is on a new line and not continuing off of the old line.
